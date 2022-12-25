81cd4f9248610f4061a22bf17742fc4fc759962a
ehwgal <ehwgaljaard@gmail.com>
2022-12-25 23:29:04 +0100

    make navbar position relative

--- src/contact.js
+++ src/contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
                 initial={{ width: 0 }}
                 animate={{ width: "70%", transition: { duration: 0.4} }}
                 exit={{ x: window.innerWidth, transition:{duration:0.2}}}
-                style={{marginBottom: "50vh"}}
+                style={{marginBottom: "40vh"}}
             >
                 <h1>Contact</h1>
                 <p className="large-font"><b>For questions, please contact:</b>&ensp; m.vanderleeden@amsterdamumc.nl</p>
